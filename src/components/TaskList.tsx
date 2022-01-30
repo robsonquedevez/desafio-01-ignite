b50eab318d974bf0ef9ad442106be3d2fe2cfc1f
Robson Quedevez <robson.quedevez@hotmail.com>
2022-01-30 16:25:11 -0300

    Challenge completed

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,14 +16,34 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if(newTaskTitle !== '') {
+      const newTask = {
+        id: Math.round(Math.random() * 10000),
+        title: newTaskTitle,
+        isComplete: false
+      }
+
+      setTasks([...tasks, newTask]);
+    }
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const tasksUpdate = tasks.map(task => {
+      if(task.id === id){
+        task.isComplete = !task.isComplete
+      }
+      return task;
+    });
+
+    setTasks(tasksUpdate);
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    const taskNotRemove = tasks.filter(task => task.id !== id);
+
+    setTasks(taskNotRemove);
   }
 
   return (
